869f9fda3a1a5a4e17fa9bf75ba4fa8f998711be
DUC_PC <minhducvu.212@gmail.com>
2018-08-07 14:13:36 +0700

    init Projet

---
+++ app/src/main/java/com/example/ducvu212/demomvp/screen/main/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ducvu212.demomvp.screen.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ducvu212.demomvp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
